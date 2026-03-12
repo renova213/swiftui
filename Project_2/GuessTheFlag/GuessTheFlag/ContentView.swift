ec2d07ce1895be3f8e12d650bb1e21fc3a406192
Rizco Renova <rizcorenova31@gmail.com>
2026-03-12 11:13:43 +0700

    guess the flag

---
+++ Project_2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,114 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Entrust Inv097 on 12/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var currentScore = 0
+    @State private var scoreTitle = ""
+    @State private var scoreMessage = ""
+    @State private var showingScore = false
+    @State private var showingGameOver = false
+    @State private var questionCount = 1
+
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct!"
+            currentScore += 1
+            scoreMessage = "Your score is now \(currentScore)."
+        } else {
+            scoreTitle = "Wrong!"
+            scoreMessage = "That's the flag of \(countries[number]).\nYour score is still \(currentScore)."
+        }
+        
+        if questionCount == 8 {
+            showingGameOver = true
+        } else {
+            showingScore = true
+        }
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        questionCount += 1
+    }
+    
+    func resetGame() {
+        currentScore = 0
+        questionCount = 0
+        askQuestion()
+    }
+
+    var body: some View {
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: .yellow, location: 0.3),
+                .init(color: .blue, location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
+
+            VStack {
+                Spacer()
+                
+                Text("Guess the Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundStyle(.white)
+
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.buttonBorder)
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(currentScore)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                
+                Spacer()
+            }
+            .padding()
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text(scoreMessage)
+        }
+        .alert("Game Over!", isPresented: $showingGameOver) {
+            Button("Restart Game", action: resetGame)
+        } message: {
+            Text("You answer correctly \(currentScore) out of 8.")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project_2/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Entrust Inv097 on 12/03/26.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
